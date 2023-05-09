05bfc67096f8cc42f733d38c11969824793bb9bb
summerjamal <summer.jamal@umlaut.com>
2023-05-09 11:43:23 -0400

    Added string addition to 15 case

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,9 +1,13 @@
 def fizzbuzz(number):
+    output_string3 = "fizz"
+    output_string5 = "buzz"
+    output_string_15 = output_string3 + output_string5
     if number % 15 == 0:
-        return "fizzbuzz"
+        return output_string_15
     elif number % 3 == 0:
-        return "fizz"
+        return output_string3
     elif number % 5 == 0:
-        return "buzz"
+        return output_string5
     else:
         return number
+
